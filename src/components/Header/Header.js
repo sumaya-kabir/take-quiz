a2b9cd6ce1b88198c12ade2392612ef74a5c1bc2
sumaya-kabir <sumayakabir4547@gmail.com>
2022-10-11 17:19:23 +0600

    header responsive done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,14 +3,16 @@ import pic from '../../img/flat-people-asking-questions-illustration_23-21489106
 const Header = () => {
     return (
         <div className='md:grid grid-cols-2'>
-            <div className='pt-36 ml-20'>
+            <div className='md:pt-36 ml-20'>
             <h2 className='text-6xl'>Take <span className='text-red-400 font-bold'>Quiz</span> <br />
                 Give answers <br />
                 and Learn together
             </h2>
             <button className='bg-red-400 text-white px-5 py-3 rounded-lg mt-6 font-bold'>Take Quiz</button>
             </div>
-            <img className='w-5/6' src={pic} alt="" />
+            <div>
+                <img className='w-5/6' src={pic} alt="" />
+            </div>
         </div>
     );
 };
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -9,14 +9,15 @@ const NavBar = () => {
                 <h2 className='text-center p-3 rounded-lg text-3xl text-white font-bold bg-red-400'>Take Quiz</h2>
             </div>
 
-            <div className='text-center nav-style text-xl font-semibold text-red-400'>
+            <nav className='text-center nav-style text-xl font-semibold text-red-400 '>
                     <NavLink to={'/'} className={({ isActive }) =>
-              isActive ? "bg-red-400 text-white rounded-xl" : undefined
-            }>Home</NavLink>
+                    isActive ? "md:bg-red-400 md:text-white rounded-xl" : undefined
+                    }>Home
+                    </NavLink>
                     <NavLink to={'/topics'}>Topics</NavLink>
                     <NavLink to={'/statistics'}>Statistics</NavLink>
                     <NavLink to={'/blog'}>Blog</NavLink>
-            </div>
+            </nav>
         </div>
     );
 };
